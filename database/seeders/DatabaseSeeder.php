b4510bef6882500e4f2e34a16591ca1909c5a154
Benaiah Yusuf <yusufbenaiah@gmail.com>
2023-08-31 00:31:12 +0100

    Add promotion resource

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +19,6 @@ class DatabaseSeeder extends Seeder
         $this->call(OrderSeeder::class);
         $this->call(FileSeeder::class);
         $this->call(PostSeeder::class);
+        $this->call(PromotionSeeder::class);
     }
 }
