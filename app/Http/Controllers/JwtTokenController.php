d4af34d40653e4020227ec5860bff2370ae56b97
Benaiah Yusuf <yusufbenaiah@gmail.com>
2023-08-30 23:17:54 +0100

    Add api resource for users and jwt token

---
+++ app/Http/Controllers/JwtTokenController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JwtToken;
+use Illuminate\Http\Request;
+
+class JwtTokenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(JwtToken $jwtToken)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, JwtToken $jwtToken)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(JwtToken $jwtToken)
+    {
+        //
+    }
+}
